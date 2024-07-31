9b79ae0fc92c59c603a737e3f592667065d112e8
Tanish Valesha <76911714+TanishValesha@users.noreply.github.com>
2024-08-01 02:14:35 +0530

    Update LoginPage.jsx

--- client/src/components/LoginPage.jsx
+++ client/src/components/LoginPage.jsx
@@ -10,7 +10,7 @@ const LoginPage = () => {
 
   async function login(e){
     e.preventDefault();
-    const response = await fetch("http://localhost:3000/login", {
+    const response = await fetch("https://blogapp-mern-server2.onrender.com/login", {
       method: 'POST',
       body: JSON.stringify({userName, password}),
       headers: {'Content-Type': 'application/json'},
